b7d709a9b41fe8d0ee735885ad3230f6e920bc1e
rg-yt <103154533+rg-yt@users.noreply.github.com>
2023-05-02 14:08:17 -0400

    Add back starting board test

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -1,4 +1,10 @@
 require './lib/piece'
 describe Piece do
-
+  subject(:piece) { described_class.new('white') }
+  describe '#initialize' do
+    it 'white piece has a character of xw' do
+      result = piece.character
+      expect(result).to eq('xw')
+    end
+  end
 end
