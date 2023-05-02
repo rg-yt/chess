78eeb168f094306b28e81c08103f3ef2b891c840
rg-yt <103154533+rg-yt@users.noreply.github.com>
2023-05-02 16:39:10 -0400

    Adjust piece class to remove row column

---
+++ lib/piece.rb
@@ -0,0 +1,42 @@
+class Piece
+  attr_reader :character, :color
+
+  def initialize(color)
+    @color = color
+    @character = 'x' << color[0]
+  end
+
+  def moveset
+    [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+  end
+
+  def get_moves(row, column, board)
+    all_moves(row, column, board)
+  end
+
+  def all_moves(row, column, board)
+    moves = []
+    moveset.each do |move|
+      moves << [row, column].zip(move).map { |n| n.reduce(&:+) }
+    end
+    inbounds_moves(moves, board)
+  end
+
+  def inbounds_moves(moves, board)
+    moves.select! do |move|
+      move.all? { |num| num.between?(0, 7) }
+    end
+    team_check(moves, board)
+  end
+
+  def team_check(moves, board)
+    moves.select! do |move|
+      board[move.first][move.last].nil? || board[move.first][move.last].color != color
+    end
+    moves
+  end
+
+  def to_s
+    character
+  end
+end
