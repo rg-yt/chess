542d19d44917ecc183d480f2f27f153720e5ee4a
rg-yt <103154533+rg-yt@users.noreply.github.com>
2023-05-02 13:09:56 -0400

    Add tests for starting board positions

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,7 +1,22 @@
+require './lib/board'
 describe Board do
   subject(:board) { described_class.new() }
   describe '#initialize' do
-    result = board.board.length
-    expect(result).to eq(8)
+    it 'retuns a board length of 8' do
+      result = board.board.length
+      expect(result).to eq(8)
+    end
+
+    it 'each row has a length of 8' do
+      result = board.board.all?{ |row| row.length == 8}
+      expect(result).to be true
+    end
+  end
+
+  describe '#starting_board' do
+    it 'changes the board' do
+      expect{board.starting_board}.to change{board.board}
+    end
+  
   end
 end
