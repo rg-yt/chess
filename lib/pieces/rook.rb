08d7f45d8ba7f04850e3e373c49b025650426462
rg-yt <103154533+rg-yt@users.noreply.github.com>
2023-05-03 14:46:26 -0400

    Add get movement method to rook

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -4,7 +4,51 @@ class Rook < Piece
     super(color)
     @character = 'r' << color[0]
   end
-  
-  def slide_moves()
+
+  def get_moves(row, column, board)
+    slide_up(row, column, board) +
+      slide_down(row, column, board) +
+      slide_left(row, column, board) +
+      slide_right(row, column, board)
+  end
+
+  def slide_up(row, column, board, array = [])
+    array << [row, column]
+    return array[1..] if row.zero?
+    if board[row][column] && board[row][column] != self
+      return board[row][column].color == color ? array[1...-1] : array[1..]
+    end
+
+    slide_up(row - 1, column, board, array)
+  end
+
+  def slide_down(row, column, board, array = [])
+    array << [row, column]
+    return array[1..] if row == board.length
+    if board[row][column] && board[row][column] != self
+      return board[row][column].color == color ? array[1...-1] : array[1..]
+    end
+
+    slide_down(row + 1, column, board, array)
+  end
+
+  def slide_left(row, column, board, array = [])
+    array << [row, column]
+    return array[1..] if column.zero?
+    if board[row][column] && board[row][column] != self
+      return board[row][column].color == color ? array[1...-1] : array[1..]
+    end
+
+    slide_left(row, column - 1, board, array)
+  end
+
+  def slide_right(row, column, board, array = [])
+    array << [row, column]
+    return array[1..] if column == board.length
+    if board[row][column] && board[row][column] != self
+      return board[row][column].color == color ? array[1...-1] : array[1..]
+    end
+
+    slide_right(row, column + 1, board, array)
   end
 end
