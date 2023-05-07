a8981d235f026afdf748d1b7b7a9579063667e07
rg-yt <103154533+rg-yt@users.noreply.github.com>
2023-05-07 18:08:36 -0400

    Add knight moves

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -4,7 +4,30 @@ class Knight < Piece
     super(color)
     @character = 'k' << color[0]
   end
-  def get_moves(*args)
-    []
+
+  def moveset
+    [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1]]
+  end
+
+  def get_moves(row, column, board)
+    moves = []
+    moveset.each do |move|
+      moves << [row, column].zip(move).map { |n| n.reduce(&:+) }
+    end
+    inbounds_moves(moves, board)
+  end
+
+  def inbounds_moves(moves, board)
+    moves.select! do |move|
+      move.all? { |num| num.between?(0, 7) }
+    end
+    team_check(moves, board)
+  end
+
+  def team_check(moves, board)
+    moves.select! do |move|
+      board[move.first][move.last].nil? || board[move.first][move.last].color != color
+    end
+    moves
   end
 end
