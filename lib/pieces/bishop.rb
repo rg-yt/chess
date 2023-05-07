3bc93a57cb39818a79dab0159d542f814915b8d9
rg-yt <103154533+rg-yt@users.noreply.github.com>
2023-05-07 17:36:58 -0400

    Add movement to bishop

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -5,7 +5,53 @@ class Bishop < Piece
     super(color)
     @character = 'b' << color[0]
   end
-  def get_moves(*args)
-    []
+
+  def get_moves(row, column, board)
+    slide_right_up(row, column, board) +
+      slide_right_down(row, column, board) +
+      slide_left_up(row, column, board) +
+      slide_left_down(row, column, board)
   end
+
+  def slide_right_up(row, column, board, array = [])
+    array << [row, column]
+    return array[1..] if row.zero? || column == 7
+    if board.dig(row, column) && board[row][column] != self
+      return board[row][column].color == color ? array[1...-1] : array[1..]
+    end
+
+    slide_right_up(row - 1, column + 1, board, array)
+  end
+
+  def slide_right_down(row, column, board, array = [])
+    array << [row, column]
+    return array[1..] if row == 7 || column == 7
+    if board.dig(row, column) && board[row][column] != self
+      return board[row][column].color == color ? array[1...-1] : array[1..]
+    end
+
+    slide_right_down(row + 1, column + 1, board, array)
+  end
+
+  def slide_left_up(row, column, board, array = [])
+    array << [row, column]
+    return array[1..] if row.zero? || column.zero?
+    if board.dig(row, column) && board[row][column] != self
+      return board[row][column].color == color ? array[1...-1] : array[1..]
+    end
+
+    slide_left_up(row - 1, column - 1, board, array)
+  end
+
+  def slide_left_down(row, column, board, array = [])
+    array << [row, column]
+    return array[1..] if row == 7 || column.zero?
+    if board.dig(row, column) && board[row][column] != self
+      return board[row][column].color == color ? array[1...-1] : array[1..]
+    end
+
+    slide_left_down(row + 1, column - 1, board, array)
+  end
+
 end
+
