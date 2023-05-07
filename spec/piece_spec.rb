8b630ff027040a310b9b49427cf0f439ad0b3527
rg-yt <103154533+rg-yt@users.noreply.github.com>
2023-05-07 17:38:20 -0400

    Update piece spec

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -7,4 +7,13 @@ describe Piece do
       expect(result).to eq('xw')
     end
   end
+
+  describe '#get_moves' do
+    let(:board) { Array.new(8) { Array.new(8) } }
+    it 'returns and array of valid moves' do
+      array = [[1, 0], [1, 1], [0, 1]].sort
+      result = piece.get_moves(0, 0, board).sort
+      expect(result).to eq(array)
+    end
+  end
 end
