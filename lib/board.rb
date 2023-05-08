52fdfef25157b417f2fec0f65afe51a43524d8da
rg-yt <103154533+rg-yt@users.noreply.github.com>
2023-05-08 14:46:42 -0400

    Adjust move_piece and select_piece for play input

--- lib/board.rb
+++ lib/board.rb
@@ -2,24 +2,32 @@ require './lib/pieces'
 
 class Board
   attr_accessor :board, :piece, :players
+
   def initialize
     @board = Array.new(8) { Array.new(8) }
     @piece = {}
-    @players = %w(white black)
   end
 
-  def select_piece(row, column)
-    piece[:row] = row
-    piece[:column] = column
-    piece[:object] = board.dig(row, column)
-    piece[:moves] = piece[:object] ? piece[:object].get_moves(row, column, board) : []
+  def select_piece(color, row = select_row, column = select_column)
+    @piece[:object] = board.dig(row, column)
+    @piece[:row] = row
+    @piece[:column] = column
+
+    select_piece(color) until @piece[:object] && @piece[:object].color == color
   end
 
-  def move_piece(row, column)
-    return unless piece[:moves].include?([row, column])
+  def move_piece(row = select_row, column = select_column)
+    return false unless include_moves(row, column)
 
     board[row][column] = piece[:object]
     board[piece[:row]][piece[:column]] = nil
+    undo_move(row, column) if in_check?
+    true
+  end
+
+  def undo_move(row, column)
+    board[row][column] = nil
+    board[piece[:row]][piece[:column]] = piece[:object]
   end
 
   def starting_board
@@ -56,8 +64,43 @@ class Board
     8.times { |num| print "  #{num}  " }
     puts ''
   end
-end
 
-board = Board.new
-board.starting_board
-board.show_board
+  def include_moves(row, column)
+    piece[:object].get_moves(piece[:row], piece[:column], board).include?([row, column])
+  end
+
+  def in_check?
+    coordintes = find_king
+    board.each_with_index do |rows, row|
+      rows.each_with_index do |char, column|
+        next if char.nil? || char.color == piece[:object].color
+        return true if char.get_moves(row, column, board).include?(coordintes)
+      end
+    end
+    false
+  end
+
+  def find_king
+    board.each_with_index do |rows, row|
+      rows.each_with_index do |char, column|
+        return [row, column] if char.is_a?(King) && char.color == piece[:object].color
+      end
+    end
+  end
+
+  def select_row
+    loop do
+      print 'Select row:'
+      value = gets.chomp.to_i
+      return value if value.between?(0, 7)
+    end
+  end
+
+  def select_column
+    loop do
+      print 'Select column:'
+      value = gets.chomp.to_i
+      return value if value.between?(0, 7)
+    end
+  end
+end
